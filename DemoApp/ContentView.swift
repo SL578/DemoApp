f23065362318a081b4ac105d863bfbf863e678b2
SL578 <linstephen578@gmail.com>
2024-01-15 15:15:12 -0800

    2nd commit

--- DemoApp/ContentView.swift
+++ DemoApp/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     @Query private var items: [Item]
 
     var body: some View {
+        // Hello
         NavigationSplitView {
             List {
                 ForEach(items) { item in
